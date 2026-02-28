78c6fd69d6fb3af7ef53befb24cbed381fcf56ef
Denis Filin <70771361+denisfilin@users.noreply.github.com>
2026-02-28 20:14:46 +0200

    Update add-product.js

--- api/add-product.js
+++ api/add-product.js
@@ -22,6 +22,9 @@ export default async function handler(req, res) {
       console.error(error)
       return res.status(500).json({ success: false, error })
     }
+    if (!title || !price) {
+  return res.status(400).json({ error: "Title and price are required" })
+}
 
     return res.status(200).json({ success: true, data })
 
