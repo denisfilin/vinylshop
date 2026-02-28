ced2bfa876e67456169c725455f6af5e12e14280
Denis Filin <70771361+denisfilin@users.noreply.github.com>
2026-02-28 10:49:04 +0200

    Update add-product.js

--- api/add-product.js
+++ api/add-product.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   }
 
   const supabaseUrl = process.env.SUPABASE_URL;
-  const supabaseKey = process.env.SUPABASE_ANON_KEY;
+  const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 
   const response = await fetch(`${supabaseUrl}/rest/v1/products`, {
     method: "POST",
