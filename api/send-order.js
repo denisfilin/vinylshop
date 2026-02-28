969ddddb02a4271ca75634f9732f1428dc2e6c38
Denis Filin <70771361+denisfilin@users.noreply.github.com>
2026-02-28 19:54:06 +0200

    Update send-order.js

--- api/send-order.js
+++ api/send-order.js
@@ -34,4 +34,9 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
+  if (!token || !chatId) {
+  return res.status(500).json({ error: "Telegram environment variables missing" });
 }
+}
+
+
